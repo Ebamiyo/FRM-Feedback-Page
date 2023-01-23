5e56a9134b943d8c9f6fc0ec6d4202ec8bb89f46
Ebamiyo Omolade <ebamiyoomolade@gmail.com>
2023-01-23 14:30:47 +0100

    adjust thankyou message functionality

--- Build/script.js
+++ Build/script.js
@@ -2,26 +2,35 @@ let submitBtn = document.getElementById('submit-btn')
 let mainDiv = document.getElementById('main-div')
 let voteNumber = document.querySelectorAll('.votenumber')
 let thankYouMessage = document.getElementById('thankyoumessage')
+let thankYouDiv = document.getElementById('thankyou-div')
+let selectAVoteDiv= document.getElementById('select-a-vote-div')
+let selectMessage = document.getElementById('selectmessage')
 
 let vote = ""
+let voteIsClicked = false
 voteNumber.forEach(votenumber => {
     votenumber.addEventListener('click', function() {
         vote = votenumber.textContent
+        voteIsClicked = true
         console.log(vote);
     })
 })
 
 let baseString = ""
-// submitBtn.addEventListener('click', function() {
-//     baseString = `You have selected ${vote} out of 5`
-//     console.log(baseString);
-//     thankYouMessage.innerHTML = `<p>${baseString}</p>`
-// })
-
-window.addEventListener('DOMContentLoaded', () => {
-    submitBtn.addEventListener('click', function() {
+submitBtn.addEventListener('click', function() {
+    // show thankyou message when a vote is selected and submitted
+    if (voteIsClicked === true) {
+        mainDiv.style.display = "none"
+        thankYouDiv.style.display = "flex"
+        thankYouDiv.style.flexDirection = "column"
+        thankYouDiv.style.justifyContent = "center"
+        thankYouDiv.style.alignItems = "center"
         baseString = `You have selected ${vote} out of 5`
         console.log(baseString);
         thankYouMessage.innerHTML = `<p>${baseString}</p>`
-    })
-});
+        // show "select a vote" message when submission is made without voting
+    } else {
+        selectAVoteDiv.style.display = "block"
+    }
+})
+
